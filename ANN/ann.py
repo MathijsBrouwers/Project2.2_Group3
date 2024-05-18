83a6c3608828d9dd673434f499f394ac579d9b8f
CrabtreeAJ <a.crabtree@student.maastrichtuniversity.nl>
2024-05-18 12:49:03 +0200

    ann update

--- ANN/ann.py
+++ ANN/ann.py
@@ -10,6 +10,6 @@ from keras.optimizers import Adam
 class Network(Sequential):
     def __init__(self):
         super(Network, self).__init__()
-        self.add(Dense(64, activation='relu', input_dim=5))
+        self.add(Dense(64, activation='relu', input_dim=6))
         self.add(Dense(1, activation='sigmoid'))
         self.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
