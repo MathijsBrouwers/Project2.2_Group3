a0ea6da311ccdb0a0a63f2395eedaa4a74a0867b
JuliusVerschoof <j.verschoof@student.maastrichtuniversity.nl>
2024-05-20 13:35:28 +0200

    stereotypes, positive generalities, and updated tokenize

--- evaluators/riskWords/riskWords.py
+++ evaluators/riskWords/riskWords.py
@@ -2,18 +2,36 @@ import riskWordsLists
 import string
 
 
-##Takes as input .txt file and gives as output only the cleaned words in an array.
-def tokenize(file_path):
+
+import re
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+def tokenize(file_path): # takes txt files and tokenizes the tweet, using some basic preporcessing techniques and spacy lemmatizes the text
     try:
-        with open(file_path, 'r',encoding="utf-8") as file:
+        with open(file_path, 'r', encoding="utf-8") as file:
             contents = file.read()
-            words = contents.split()
-            translation_table = str.maketrans('','', string.punctuation)
-            return [word.translate(translation_table).lower() for word in words]
+            
+           
+            contents = contents.lower()
+            contents = re.sub(r'http\S+|www\S+|https\S+', '', contents, flags=re.MULTILINE)
+            contents = re.sub(r'#\w+', '', contents)
+    
+            
+            doc = nlp(contents)
+            
+    
+            cleaned_words = [
+                token.lemma_ for token in doc 
+                if not token.is_stop and not token.is_punct and not token.like_num and token.is_alpha
+            ]
+            return cleaned_words
+
     except FileNotFoundError:
         print('File not found error')
         return None
-    
+
 
 def get_ratio_of_risk_words(file_path, amount_of_risk_words):
     try:
@@ -42,11 +60,11 @@ def count_risk_words(cleaned_list, words):
 
 
 
-amount_of_loaded_words =  count_risk_words(tokenize('evaluators\\riskWords\\testFile.txt'), riskWordsLists.loaded_language)
-print('Amount of loaded words: ', amount_of_loaded_words)
-print('This gives ratio: ', get_ratio_of_risk_words('evaluators\\riskWords\\testFile.txt', amount_of_loaded_words ))
+#amount_of_loaded_words =  count_risk_words(tokenize('evaluators\\riskWords\\testFile.txt'), riskWordsLists.loaded_language)
+#print('Amount of loaded words: ', amount_of_loaded_words)
+#print('This gives ratio: ', get_ratio_of_risk_words('evaluators\\riskWords\\testFile.txt', amount_of_loaded_words ))
 
-print('Amount of emotional words: ', count_risk_words(tokenize('evaluators\\riskWords\\testFile.txt'), riskWordsLists.emotional_language))
+#print('Amount of emotional words: ', count_risk_words(tokenize('evaluators\\riskWords\\testFile.txt'), riskWordsLists.emotional_language))
 
 
 
