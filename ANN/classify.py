a203105fe3503430dbef58451d6009827075a8a6
CrabtreeAJ <a.crabtree@student.maastrichtuniversity.nl>
2024-06-11 13:22:51 +0200

    code modification no data removal

---
+++ ANN/classify.py
@@ -0,0 +1,59 @@
+# ----------------------------------------------------------------------
+# This file when run will ask to be provided with a text. Upon recieving it the text will be classified by the ann
+# as propaganda or non-propaganda. To run this file on must put "python -m ANN.classify" into the terminal, then 
+# enter the text you wish to classify when prompted.
+# ----------------------------------------------------------------------
+
+import numpy as np
+from evaluators.emotionEval import EmotionEval
+from evaluators.Positive_Generalities_and_stereotypes.stereotypes import Stereotypes
+from evaluators.Positive_Generalities_and_stereotypes.pos_gen import PosGen
+from evaluators.riskWords.riskWords import RiskWords
+from evaluators.riskWords.constants import EMOTIONAL_LANGUAGE, LOADED_LANGUAGE, BANDWAGON_LANGUAGE
+from evaluators.riskWords.riskWordsLists import emotional_language, loaded_language, bandwagon_language
+
+from .predict import classify
+
+# Create instances of evaluators
+emotionEvaluator = EmotionEval()
+stereotypeEvaluator = Stereotypes()
+posgenEvaluator = PosGen()
+riskWords = RiskWords()
+
+def compile_evaluations(file_path, evaluators):
+    results = []
+    for evaluator in evaluators:
+        result = evaluator.evaluate(file_path)
+        results.append(result)
+
+    results.append(riskWords.evaluate(file_path, emotional_language, EMOTIONAL_LANGUAGE))
+    results.append(riskWords.evaluate(file_path, loaded_language, LOADED_LANGUAGE))
+    results.append(riskWords.evaluate(file_path, bandwagon_language, BANDWAGON_LANGUAGE))
+
+    print(results)
+
+    return np.array(results)
+
+def process_text(text, evaluators):
+    temp_file_path = "temp_text.txt"
+    with open(temp_file_path, "w") as file:
+        file.write(text)
+
+    feature_vector = compile_evaluations(temp_file_path, evaluators)
+
+    print(feature_vector.shape)
+
+    np.save("feature_vector.npy", feature_vector)
+    
+
+
+if __name__ == "__main__":
+
+    text = input("Please enter the text to be evaluated: ")
+    
+    evaluators = [emotionEvaluator, stereotypeEvaluator, posgenEvaluator]
+    
+    process_text(text, evaluators)
+    classify()
+    #y_pred = predict(feature_vector)
+    #print("Doc prediction", y_pred)
--- ANN/weight_test.py
+++ ANN/weight_test.py
@@ -1,3 +1,7 @@
+# ----------------------------------------------------------------------
+# This file simply checks the precision and recall of the modified artificial neural network classifier. To use
+# just run the file.
+# ----------------------------------------------------------------------
 
 from keras.models import load_model
 import numpy as np
@@ -6,11 +10,13 @@ from sklearn.metrics import precision_score, recall_score
 X_test = np.load('DATASETS/X_test.npy')
 y_test = np.load('DATASETS/y_test.npy')
 
+X_test_modified = X_test[:, :-1]
+
 
 prop_model = load_model('ANN\\prop_weight_model.h5')
 
 
-y_pred_prob = prop_model.predict(X_test)
+y_pred_prob = prop_model.predict(X_test_modified)
 y_pred = (y_pred_prob > 0.5).astype(int)
 
 precision = precision_score(y_test, y_pred)
