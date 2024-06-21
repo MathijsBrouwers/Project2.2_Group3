df0f696451208d372d973c268d242c0cd3b6fbd2
JuliusVerschoof <j.verschoof@student.maastrichtuniversity.nl>
2024-06-21 15:49:56 +0200

    Stereotypes changes final

--- evaluators/stereotypes.py
+++ evaluators/stereotypes.py
@@ -1,98 +1,87 @@
-# THIS BY ITSELF IS NOT A GOOD PROPAGANDA DETECTOR, it tries to detect stereotypes by checking through commonly used steretype wordss and sentiment analysis of words
-#then checks if the words are close to entities if they are it would increase the score. the score is from 0 to 1, 0 meaning no stereotypes and 1 a lot
+# now works witht he chatpgt api, ask thh prompt you can see at line 34. I tried fine tuning the promp. I copied the framework we had for emotionEval and changed the prompt to 
+
+# ----------------------------------------------------------------------
+# This file is meant to run by taking in the file path to a text file via the evaluate method
+# and will return a score from 0-1 to represent how much emotionally charged language is in the text
+# with 0 being the least and 1 being the most
+# ----------------------------------------------------------------------
 
 
-
-# anyway you can run this by using the evaluate method, which needs an input of a txt file and returns a score of 0-1 for that txt file 0 meaning no postive generalites 1 alot. 
-
-
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from openai import OpenAI
 import re
-import spacy
+from .abstractEvaluator import abstractEvaluator
 
-from evaluators.abstractEvaluator import abstractEvaluator
+# Unique api key must be placed here in order to communicate with gpt
+api_key = ""
+client = OpenAI(api_key=api_key)
 
-nlp = spacy.load('en_core_web_sm')
+# This class implements the abstractEvaluator class
+class EmotionEval(abstractEvaluator): 
 
-class Stereotypes(abstractEvaluator):
-    def function(self, x):# tries to change the representation of the ratio a bit to a bit more significant, might want to change this into smth better
-        return x * (2 - x)
 
-    def tokenize(self, file_path): # takes txt files and tokenizes the tweet, using some basic preporcessing techniques and spacy lemmatizes the text
-        try:
-            with open(file_path, 'r', encoding="utf-8") as file:
-                contents = file.read()
-                
-            #make lowercase remove links and remove # (idk if they actually had any but just in case i guess) 
-                contents = contents.lower()
-                contents = re.sub(r'http\S+|www\S+|https\S+', '', contents, flags=re.MULTILINE)
-                contents = re.sub(r'#\w+', '', contents)
+     # This method makes the actual call to gpt-3.5-turbo. It contains its instruction and will also feeed it a text passage to work with.
+    # Additionally this method writes gpt's output into the output.txt file for later usage
+    def callChat(self, text):
+        article = open("evaluators\\TestArticle.txt","r").read()
+        if text is None:
+            text = article
+
+
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": ("The following texts you will receive will be tweet or news articles that may or may not contain elements of propaganda within."
+                                                "For each text I would like you to extract and return exact sentences from the article or tweet that contain examples of "
+                                                "a stereotype, please number each one. A stereotype is a oversimplified or generalized belief about a group "
+                                                "of people based on their characteristics such as race, ethnicity, gender, or religion."
+                                                "Please only flag actual stereoptypes which are generalizing groups of people."
+                                                "Avoid flagging statements that are merely negative descriptions about groups"
+                                                "If you do not find any such examples then say 'I found none'. Additionally please "
+                                                "explain your reasoning for choosing each example. The text is:")},
+                    {"role": "user", "content": text}], 
+            stream=True, seed = 2, temperature = 0
+        )
         
-                doc = nlp(contents)
-                #Perfroms lemmatization 
-                cleaned_words = [
-                    token.lemma_ for token in doc 
-                    if not token.is_stop and not token.is_punct and not token.like_num and token.is_alpha
-                ]
+        with open("evaluators\\output.txt", "w") as file:
+           
             
-                return cleaned_words
-
-        except FileNotFoundError:
-            print('File not found error')
-            return None
-        
-    def detect_stereotypes(self, tokens):
-        #gpt gave me this list 
-        stereotypes = ["lazy", "violent", "aggressive", "dumb", "criminal", "thug", "terrorist", "greedy", "untrustworthy", "ignorant", "savage", "uncivilized", "backward", "inferior", "exotic", "submissive", "promiscuous", "oppressed", "primitive", "barbaric", "dangerous", "unclean", "fanatical", "militant", "radical", "illegal", "alien", "parasite", "menace", "welfare queen", "trailer trash", "gang member", "thief", "druggie", "jihadist"]
-        detected_stereotypes = []
-
-        sentiment_analyzer = SentimentIntensityAnalyzer()  # Initialize sentiment analyzer
-
-        for i, token in enumerate(tokens): #loops through the tokens to check if they are a known stereotype word
-            if token in stereotypes:
-                # Check if the stereotype word is close to entities, the idea being that if the word is found clsoe to an entity the chance of if being a steretype is higher. 
-                #need to experiment with the range I look through 
-                for entity in nlp(" ".join(tokens[max(0, i - 4): min(i + 5, len(tokens))])).ents:
-                    if entity.label_ in ["PERSON", "NORP", "ORG"]:
-                        detected_stereotypes.append((token, entity.text, "Entity"))
-                        break  
-
-            else:
-                #checks fof words with negative sentiment the idea being that these are very negative words possibly used to steretype groups of people 
-                token_sentiment = sentiment_analyzer.polarity_scores(token)
-                if token_sentiment['compound'] < -0.6: #might have to adjust this value 
-                    entity_found = False
-                    for entity in nlp(" ".join(tokens[max(0, i - 4): min(i + 5, len(tokens))])).ents:
-                        if entity.label_ in ["PERSON", "NORP", "ORG"]:
-                            detected_stereotypes.append((token, entity.text, "Entity"))
-                            entity_found = True
-                            break  
+            for chunk in response:
                 
-                    if not entity_found:
-                        detected_stereotypes.append((token, "Negative sentiment", "Sentiment"))
+                if chunk.choices[0].delta.content is not None:
+                    #print(chunk.choices[0].delta.content, end="")
+                    file.write(chunk.choices[0].delta.content)
 
-        return detected_stereotypes
+            file.write("\n")
+            
+    # This method counts the toal number of sentences within the text passage that was given to gpt
+    def count_sentences(self, text):
+        sentences = re.split(r'[.!?]', text)
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        return len(sentences)
 
-    def calculate_stereotype_score(self, detected_stereotypes, tokens):
-        #I give more value to the steretypes describing an entity as the likelihood the word is used in the contetx of being a steretype is higher.  
-        entity_stereotype_weight = 1  # Weight for stereotypes found describing an entity
-        non_entity_stereotype_weight = 0.5  # Weight for stereotypes found without entity
-
-        total_entity_stereotypes = sum(1 for stereotype, context, context_type in detected_stereotypes if context_type == "Entity")
-        total_non_entity_stereotypes = len(detected_stereotypes) - total_entity_stereotypes
-
+    # This method counts the examples provided by gpt by reading its output text from the output.txt file
+    def count_examples(self):
+        examples = 0
+        with open("evaluators\\outputStereo.txt", 'r') as file:
+            for line in file:
+                if line.strip() == '':
+                    examples += 1
         
-        total_words = len(tokens)
-        if total_words > 0:
-            score = (total_entity_stereotypes * entity_stereotype_weight + total_non_entity_stereotypes * non_entity_stereotype_weight) / total_words
-        else:
-            score = 0  
-        return self.function(score)
-
-    def evaluate(self, file_path):
-        tokens = self.tokenize(file_path)
-        stereotypes= self.detect_stereotypes(tokens)
-        score = self.calculate_stereotype_score(stereotypes,tokens)
-        return score
+        return examples
+    
+    # This method simply uses a file path to read the text conent within the file 
+    def read_text_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        return content
 
 
+
+    # This method takes in a filepath and will use it to call gpt and create a score from 0-1 to be returned
+    def evaluate(self, file):
+
+        text = self.read_text_file(file)
+
+        self.callChat(text)
+        totalSentences = self.count_sentences(text)
+        exampleSentences = self.count_examples()
+        return (exampleSentences/totalSentences)
